bc49faabf13c766fdbb9feade9463545bc10dcf7
LAVE KUMAR <65617075+LaveKumar@users.noreply.github.com>
2025-09-16 18:14:51 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import streamlit as st 
-import numpy as np 
-import tensorflow as tf 
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder 
-import pandas as pd 
-import pickle 
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
 
 
 
